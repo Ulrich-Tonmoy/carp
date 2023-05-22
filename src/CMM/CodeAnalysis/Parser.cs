3d8635546546804fec655d8099b9503b6f95549a
Ulrich <tonmoyulrich@gmail.com>
2023-05-22 18:04:05 +0600

    chore: moved precedence to SyntaxFacts.cs

--- src/CMM/CodeAnalysis/Parser.cs
+++ src/CMM/CodeAnalysis/Parser.cs
@@ -69,7 +69,7 @@
             var left = ParsePrimaryExpression();
             while (true)
             {
-                var precedence = GetBinaryOperatorPrecedence(Current.Kind);
+                var precedence = Current.Kind.GetBinaryOperatorPrecedence();
 
                 if (precedence == 0 || precedence <= parentPrecedence)
                     break;
@@ -80,23 +80,6 @@
             }
 
             return left;
-        }
-
-        private static int GetBinaryOperatorPrecedence(SyntaxKind kind)
-        {
-            switch (kind)
-            {
-                case SyntaxKind.StarToken:
-                case SyntaxKind.SlashToken:
-                    return 2;
-
-                case SyntaxKind.PlusToken:
-                case SyntaxKind.MinusToken:
-                    return 1;
-
-                default:
-                    return 0;
-            }
         }
 
         private ExpressionSyntax ParsePrimaryExpression()
