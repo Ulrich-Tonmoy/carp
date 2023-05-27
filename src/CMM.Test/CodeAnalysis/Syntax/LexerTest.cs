112842c3aa2fd0232bfb2d9a24b1e6ead7b4c922
Tonmoy <ulrichtonmoy1@gmail.com>
2023-05-28 01:40:42 +0600

    chore: test project added

---
+++ src/CMM.Test/CodeAnalysis/Syntax/LexerTest.cs
@@ -0,0 +1,32 @@
+﻿using CMM.CodeAnalysis.Syntax;
+
+namespace CMM.Test.CodeAnalysis.Syntax
+{
+    public class LexerTest
+    {
+        [Theory]
+        [MemberData(nameof(GetTokensData))]
+        public void Lexer_Lexes_Token(SyntaxKind kind, string text)
+        {
+            var tokens = SyntaxTree.ParseTokens(text);
+            var token = Assert.Single(tokens);
+            Assert.Equal(kind, token.Kind);
+            Assert.Equal(text, token.Text);
+        }
+
+        public static IEnumerable<object[]> GetTokensData()
+        {
+            foreach (var t in GetTokens())
+                yield return new object[] { t.kind, t.text };
+        }
+
+        private static IEnumerable<(SyntaxKind kind, string text)> GetTokens()
+        {
+            return new[]
+            {
+                (SyntaxKind.IdentifierToken,"a"),
+                (SyntaxKind.IdentifierToken,"abc")
+            };
+        }
+    }
+}
