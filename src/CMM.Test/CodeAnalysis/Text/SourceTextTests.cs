004f65a5ebf27bb84998bc81112f12d80cab3510
Ulrich <tonmoyulrich@gmail.com>
2023-06-06 16:19:55 +0600

    test: add test coverage for multi line text

---
+++ src/CMM.Test/CodeAnalysis/Text/SourceTextTests.cs
@@ -0,0 +1,17 @@
+﻿using CMM.CodeAnalysis.Text;
+
+namespace CMM.Test.CodeAnalysis.Text
+{
+    public class SourceTextTests
+    {
+        [Theory]
+        [InlineData(".", 1)]
+        [InlineData(".\r\n", 2)]
+        [InlineData(".\r\n\r\n", 3)]
+        public void SourceText_IncludesLastLine(string text, int exptectedLineCount)
+        {
+            var sourceText = SourceText.From(text);
+            Assert.Equal(exptectedLineCount, sourceText.Lines.Length);
+        }
+    }
+}
