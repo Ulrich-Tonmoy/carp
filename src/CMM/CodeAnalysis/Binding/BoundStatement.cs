97efb8a53087b911ecf3f533e62de9df68297396
Tonmoy <ulrichtonmoy1@gmail.com>
2023-07-11 03:02:50 +0600

    chore: move BoundIfStatement to its own file

--- src/CMM/CodeAnalysis/Binding/BoundStatement.cs
+++ src/CMM/CodeAnalysis/Binding/BoundStatement.cs
@@ -16,18 +16,4 @@
             Initializer = initializer;
         }
     }
-    internal sealed class BoundIfStatement : BoundStatement
-    {
-        public BoundExpression Condition { get; }
-        public BoundStatement ThenStatement { get; }
-        public BoundStatement ElseStatement { get; }
-        public override BoundNodeKind Kind => BoundNodeKind.IfStatement;
-
-        public BoundIfStatement(BoundExpression condition, BoundStatement thenStatement, BoundStatement elseStatement)
-        {
-            Condition = condition;
-            ThenStatement = thenStatement;
-            ElseStatement = elseStatement;
-        }
-    }
 }
