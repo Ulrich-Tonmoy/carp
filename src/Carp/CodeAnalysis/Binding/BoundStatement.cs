d5b7b4147bde3966890c3a561a77fdecf2368e6b
Ulrich <ulrichtonmoy1@gmail.com>
2024-01-07 01:51:45 +0600

    moved BoundVariableDeclaration to separate file

--- src/Carp/CodeAnalysis/Binding/BoundStatement.cs
+++ src/Carp/CodeAnalysis/Binding/BoundStatement.cs
@@ -4,16 +4,4 @@
     {
 
     }
-    internal sealed class BoundVariableDeclaration : BoundStatement
-    {
-        public VariableSymbol Variable { get; }
-        public BoundExpression Initializer { get; }
-        public override BoundNodeKind Kind => BoundNodeKind.VariableDeclaration;
-
-        public BoundVariableDeclaration(VariableSymbol variable, BoundExpression initializer)
-        {
-            Variable = variable;
-            Initializer = initializer;
-        }
-    }
 }
