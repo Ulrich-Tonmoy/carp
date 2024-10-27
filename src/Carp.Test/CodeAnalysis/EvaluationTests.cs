3f0c668f2d4d9a6d42ef999f6a79feb1c5ac6a0c
Tonmoy <ulrichtonmoy1@gmail.com>
2024-10-27 09:23:19 +0600

    🧪test: for do-while statements

--- src/Carp.Test/CodeAnalysis/EvaluationTests.cs
+++ src/Carp.Test/CodeAnalysis/EvaluationTests.cs
@@ -69,6 +69,7 @@ namespace Carp.Test.CodeAnalysis
         [InlineData("{ var i = 10 var result = 0 while i > 0 { result = result + i i = i - 1} result }", 55)]
         [InlineData("{ var result = 0 for i = 1 to 10 { result = result + i } result }", 55)]
         [InlineData("{ var a = 10 for i = 1 to (a = a - 1) { } a }", 9)]
+        [InlineData("{ var a = 0 do a = a + 1 while a < 10 a}", 10)]
         public void Evaluator_Computes_CorrectValues(string text, object expectedValue)
         {
             AssertValue(text, expectedValue);
@@ -89,7 +90,7 @@ namespace Carp.Test.CodeAnalysis
             ";
 
             var diagnostics = @"
-                Variable 'x' is already declared.
+                'x' is already declared.
             ";
 
             AssertDiagnostics(text, diagnostics);
@@ -142,6 +143,25 @@ namespace Carp.Test.CodeAnalysis
 
             var diagnostics = @"
                 Cannot convert type 'bool' to 'int'.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_DoWhileStatement_Reports_CannotConvert()
+        {
+            var text = @"
+                {
+                    var x = 0
+                    do
+                        x = 10
+                    while [10]
+                }
+            ";
+
+            var diagnostics = @"
+                Cannot convert type 'int' to 'bool'.
             ";
 
             AssertDiagnostics(text, diagnostics);
@@ -272,6 +292,23 @@ namespace Carp.Test.CodeAnalysis
 
             var diagnostics = @"
                 Cannot convert type 'bool' to 'int'.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Variables_Can_Shadow_Functions()
+        {
+            var text = @"
+                {
+                    let print = 42
+                    [print](""test"")
+                }
+            ";
+
+            var diagnostics = @"
+                Function 'print' doesn't exist.
             ";
 
             AssertDiagnostics(text, diagnostics);
