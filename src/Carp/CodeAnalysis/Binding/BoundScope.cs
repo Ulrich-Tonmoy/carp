b3c6c6182180363795e4230b2ca1dba316707c72
Tonmoy <ulrichtonmoy1@gmail.com>
2024-10-27 09:26:02 +0600

    ♻️refactor: unified namespace for all symbol types

--- src/Carp/CodeAnalysis/Binding/BoundScope.cs
+++ src/Carp/CodeAnalysis/Binding/BoundScope.cs
@@ -5,8 +5,7 @@ namespace Carp.CodeAnalysis.Binding
 {
     internal sealed class BoundScope
     {
-        private Dictionary<string, VariableSymbol> _variables;
-        private Dictionary<string, FunctionSymbol> _functions;
+        private Dictionary<string, Symbol> _symbols;
 
         public BoundScope Parent { get; }
 
@@ -16,64 +15,61 @@ namespace Carp.CodeAnalysis.Binding
         }
 
         public bool TryDeclareVariable(VariableSymbol variable)
-        {
-            if (_variables == null) _variables = new Dictionary<string, VariableSymbol>();
-
-            if (_variables.ContainsKey(variable.Name)) return false;
-
-            _variables.Add(variable.Name, variable);
-            return true;
-        }
-
-        public bool TryLookupVariable(string name, out VariableSymbol variable)
-        {
-            variable = null;
-
-            if (_variables != null && _variables.TryGetValue(name, out variable)) return true;
-
-            if (Parent == null) return false;
-
-            return Parent.TryLookupVariable(name, out variable);
-        }
+         => TryDeclareSymbol(variable);
 
         public bool TryDeclareFunction(FunctionSymbol function)
-        {
-            if (_functions == null)
-                _functions = new Dictionary<string, FunctionSymbol>();
+            => TryDeclareSymbol(function);
 
-            if (_functions.ContainsKey(function.Name))
+        private bool TryDeclareSymbol<TSymbol>(TSymbol symbol)
+           where TSymbol : Symbol
+        {
+            if (_symbols == null)
+                _symbols = new Dictionary<string, Symbol>();
+            else if (_symbols.ContainsKey(symbol.Name))
                 return false;
 
-            _functions.Add(function.Name, function);
+            _symbols.Add(symbol.Name, symbol);
             return true;
         }
 
-        public bool TryLookupFunction(string name, out FunctionSymbol function)
-        {
-            function = null;
+        public bool TryLookupVariable(string name, out VariableSymbol variable) => TryLookupSymbol(name, out variable);
 
-            if (_functions != null && _functions.TryGetValue(name, out function))
-                return true;
+        public bool TryLookupFunction(string name, out FunctionSymbol function)
+       => TryLookupSymbol(name, out function);
+
+        private bool TryLookupSymbol<TSymbol>(string name, out TSymbol symbol)
+           where TSymbol : Symbol
+        {
+            symbol = null;
+
+            if (_symbols != null && _symbols.TryGetValue(name, out var declaredSymbol))
+            {
+                if (declaredSymbol is TSymbol matchingSymbol)
+                {
+                    symbol = matchingSymbol;
+                    return true;
+                }
+
+                return false;
+            }
 
             if (Parent == null)
                 return false;
 
-            return Parent.TryLookupFunction(name, out function);
+            return Parent.TryLookupSymbol(name, out symbol);
         }
 
-        public ImmutableArray<VariableSymbol> GetDeclaredVariables()
-        {
-            if (_variables == null)
-                return ImmutableArray<VariableSymbol>.Empty;
-            return _variables.Values.ToImmutableArray();
-        }
+        public ImmutableArray<VariableSymbol> GetDeclaredVariables() => GetDeclaredSymbols<VariableSymbol>();
 
-        public ImmutableArray<FunctionSymbol> GetDeclaredFunctions()
-        {
-            if (_functions == null)
-                return ImmutableArray<FunctionSymbol>.Empty;
+        public ImmutableArray<FunctionSymbol> GetDeclaredFunctions() => GetDeclaredSymbols<FunctionSymbol>();
 
-            return _functions.Values.ToImmutableArray();
+        private ImmutableArray<TSymbol> GetDeclaredSymbols<TSymbol>()
+            where TSymbol : Symbol
+        {
+            if (_symbols == null)
+                return ImmutableArray<TSymbol>.Empty;
+
+            return _symbols.Values.OfType<TSymbol>().ToImmutableArray();
         }
     }
 }
